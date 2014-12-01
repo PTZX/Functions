9c48100e20f7c96d886bb42b8fa963d1d5e1ba7a
41378 <41378@PC004147.coll.lr.local>
2014-12-01 10:26:46 +0000

    Calculate basic pay with all definitions. not working

--- Calculate basic pay.py
+++ Calculate basic pay.py
@@ -1,72 +1,45 @@
-<<<<<<< HEAD
+print("This program will calculate your wages, salary, untaxed income, occupational payout, earnings:")
+
+#Calculate pay
+def calculate_pay(hours,rate):
+      hours = Getting_the_things(hours)
+      rate = Getting_the_things(rate)
+
 
 #Get rate and hours
-def Getting_the_things():
-         hours = int(input("Your Hours: "))
-         rate = int(input("Hourly Rate: "))
+def Getting_the_things(hours,rate):
+      hours = int(input("Your Hours: "))
+      rate = int(input("Hourly Rate: "))
+      return hours, rate
 
 
 #Calculate total pay
 
 def calculate_total_pay(hours,rate):
-         if hours <= 40:
-                 total = calculate_basic_pay(hours,rate)
-         else:
-                  total = calculate_overtime_pay(hours,rate)
-         return total
+      if hours <= 40:
+           total = calculate_basic_pay(hours,rate)
+      else:
+            total = calculate_overtime_pay(hours,rate)
+      return total
 
 
-#overtime pay
-
-def calculate_overtime_pay(hours,rate):
-         overtime_hours = hours - 40
-         basic_pay = 40 * rate
-         total = rate * overtime_hours * 1.5
-         return total
-
-
-#calculate basic pay
+#Calculate basic pay
 
 def calculate_basic_pay(hours,rate):
-         total = hours * rate
-         return total
+      total = hours * rate
+      return total
 
 
-
-#main program
-
-total_pay = calculate_basic_pay(hours,rate)
-print("£{0} a week".format(total_pay))
-total_pay = calculate_overtime_pay(41,5)
-print("£{0} a week".format(total_pay))
-=======
-#calculate_basic_pay
-
-def calculate_basic_pay(hours,rate):
-    total = hours * rate
-    return total
-
-
-#overtime_pay
+#Overtime pay
 
 def calculate_overtime_pay(hours,rate):
-    overtime_hours = hours - 40
-    overtime = overtime_rate * hours * 1.5
-    total = overtime + total_pay
-    return total
+      overtime_hours = hours - 40
+      basic_pay = 40 * rate
+      total = rate * overtime_hours * 1.5
+      return total, hours, rate
 
-def calculate_total_pay(hours,rate):
-    if hours <= 40:
-        total = calculate
-        return total
-
-
-
-#main program
-total_pay = calculate_basic_pay(12,2)
-print(total_pay)
-
-total_pay = calculate_ovetime_pay(41,1)
-print(total_pay)
->>>>>>> branch 'master' of https://github.com/PTZX/Functions.git
-
+      
+total  = hours * rate
+#main program - display total pay
+def display_total_pay(total):
+      print("£{0}".format(total))
