5d7d977a42680d9ea4b4a84328a38eee287d8fcb
PTZX <41378@longroad.ac.uk>
2015-01-07 00:29:49 +0000

    Limiting on Lists

---
+++ lol.py
@@ -0,0 +1,10 @@
+def convertDir():
+    direction = int(input("Enter either 1 for metric to imperial or 2 for imperial to metric: "))
+    value = float(input("Enter"))
+
+    ##if direction == 1:
+        
+
+    
+def convert2inch(direction, value):
+    if 
